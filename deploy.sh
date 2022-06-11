cacb7b858f354166d558e9f7e2ac172f2e47e119
Freya-Jheng <freyajheng@gmail.com>
2022-06-11 12:44:54 +0800

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,13 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
-# git checkout -b main
 git checkout -b main
-git branch -A main
+# git branch -A main
 
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f https://github.com/<USERNAME>/<USERNAME>.GitHub.io.git main
-
-# if you are deploying to https://<USERNAME>.Github.io/<REPO>
-# git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
 git push -f https://github.com/Freya-Jheng/my-todos-vue.git main:gh-pages
 
 cd -
